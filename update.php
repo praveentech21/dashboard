2551a15bba772d3c5b26d34a2d5062953be5fb1b
Praveentech21 <ravikuar_csd@srkrec.edu.in>
2023-04-04 11:14:46 +0530

    Logo Changed Shiva

--- update.php
+++ update.php
@@ -21,7 +21,7 @@ $run = mysqli_query($con,"select * from players where status=0");
     <div class="container-scroller">
     <nav class="navbar default-layout-navbar col-lg-12 col-12 p-0 fixed-top d-flex flex-row">
         <div class="text-center navbar-brand-wrapper d-flex align-items-center justify-content-center">
-          <a class="navbar-brand brand-logo" href="index.php"><img src="assets/images/logo.png" alt="logo" /></a>
+          <a class="navbar-brand brand-logo" href="index.php"><img src="Bhavani/img/logo.png" alt="logo" /></a>
         </div>
         <div class="navbar-menu-wrapper d-flex align-items-stretch">
           <ul class="navbar-nav navbar-nav-right">
